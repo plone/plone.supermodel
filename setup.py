8936cb29ec8a036941ff7ac4945e786ee64edcf7
David Glick <davidglick@groundwire.org>
2011-05-20 17:06:27 +0000

    Prepare plone.supermodel 1.0.3.
    svn path=/plone.supermodel/trunk/; revision=49860

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.3dev'
+version = '1.0.3'
 
 long_description = (
     read('README.txt')
