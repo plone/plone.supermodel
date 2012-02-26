866d0ea208a0120a8056052b1df95e13a5d6abbc
David Glick <dglick@gmail.com>
2012-02-25 16:59:23 -0800

    argh, fix sublime's default tab indentation

--- plone/supermodel/debug.py
+++ plone/supermodel/debug.py
@@ -3,10 +3,10 @@ import threading
 
 class SupermodelDebugInfo(threading.local):
 
-	def __getattr__(self, name):
-		if name == 'stack':
-			self.stack = [None]
-			return self.stack
-		return super(SupermodelDebugInfo, self).__getattr__(name)
+    def __getattr__(self, name):
+        if name == 'stack':
+            self.stack = [None]
+            return self.stack
+        return super(SupermodelDebugInfo, self).__getattr__(name)
 
 debuginfo = SupermodelDebugInfo()
