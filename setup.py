afcbe7b34fd9047d3f006b14a3292d3894aa26b3
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:21:13 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -28,10 +28,15 @@ setup(
     version=version,
     description="Serialize Zope schema definitions to and from XML",
     long_description=long_description,
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone : 4.3",
+        "Framework :: Plone : 5.0",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
     ],
