eb33b465c82cce13178d9a8b0a0a6348fc740c92
Gil Forcada Codinachs <gforcada@gnome.org>
2018-11-11 10:38:22 +0100

    Update trove classifiers
    [ci-skip]

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
     ],
